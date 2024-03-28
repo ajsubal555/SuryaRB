84691857807a2aa008412cd885bef99d7222cc5c
ajsubal555 <130387786+ajsubal555@users.noreply.github.com>
2024-03-28 22:06:45 +0530

    Hello

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // File://home/rose/BOT/SuryaRB/config.js
 export const Config = {
-	phone_number: "6281278380339",
-	owners: ["628385818401", "+62 887-3133-561"],
+	phone_number: "918005453017",
+	owners: ["918005453017"],
 	use_pairing_code: true,
 	pairing_wait: 1000 * 6,
 
 	// TODO: FIX PREFIXES ERROR
 	prefix: ["!", ".", ""],
 
-	itsrose_apikey: process.env.ITSROSE_APIKEY || "Trial_Account_10K",
+	itsrose_apikey: process.env.ITSROSE_APIKEY || "AcRJNhdThOD76dhuyI0yzUuSGF8bYlBkZB7gKFkn9etdFRsD0BCPq4bOB1DVkjZ3",
 
 	database: {
-		use_mongo: false,
+		use_mongo: True,
 		mongo_url: "mongodb://localhost:27017/database",
 
 		path: "./database.json",
